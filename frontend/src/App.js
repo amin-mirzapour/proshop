bfd24089f44d2632d3419b7396334b7ace558638
Amin Mirzapour <aminmrzpr@gmail.com>
2020-11-30 12:27:20 +0330

    third

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/common/Footer";
+import Header from "./components/common/Header";
 
-function App() {
+const App = () => {
   return (
     <>
-     
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>welcom</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
