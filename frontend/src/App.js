c78ba850bf61481f48eb8c48dfc52a1a93a341df
Amin Mirzapour <aminmrzpr@gmail.com>
2020-12-05 09:44:07 +0330

    15 azar first

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import {BrowserRouter, Route} from 'react-router-dom';
 import Footer from "./components/common/Footer";
 import Header from "./components/common/Header";
+import HomeScreen from "./components/screens/HomeScreen";
+import ProductScreen from "./components/screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>welcom</h1>
+          <Route path='/' component={HomeScreen} exact/>
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 };
 
